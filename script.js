34eb4c0ae6710a9fd62fe1aa445be33e9f4b6030
nelson <nelsonoghenero@gmail.com>
2023-07-28 01:55:59 +0100

    added all the project files

---
+++ script.js
@@ -0,0 +1,47 @@
+const inputBox = document.getElementById("input-box");
+const listContainer = document.getElementById('list-container');
+
+
+function addTask(){
+    if (inputBox.value === ""){
+        alert("Please enter a task!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e){
+    if (e.target.tagName === "LI" ){
+        e.target.classList.toggle("checked");
+        saveData()
+    }
+    else if (e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+}, false )
+
+// any time we refresh the page , the toDo list jumps Off 
+// to stop that we create a function
+
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML)
+}
+
+
+//we need to save the data
+//whenever we open the page
+
+function showTask(){
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+
+showTask()
